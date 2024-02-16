500b65943a611f7096c76b5e30a243f72a5fb08a
marina-mmap <marinapereira.mmap@gmail.com>
2024-02-16 13:14:18 -0300

    js completo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,45 @@
+/* Case Sensitive  = reconhece letra maiúsculas e minúsculas 
+
+Por Tag: getElementByTagName()
+Por Id: getElementById()
+Por Nome: getElementsByName()
+Por Classe: getElementsByClassName()
+Por Seletor: querySelector ()
+*/
+
 let nome = window.document.getElementById('nome')
+let email= document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+
+function validaNome(){
+    let txtNome = document.querySelector('#txtNome')
+    if(nome.value.lenght<3){
+      txtNome.innerHTML = "NOME INVÁLIDO"
+      txtNome.style.color = "red"
+    }else{
+        txtNome.innerHTML = "NOME VÁLIDO" 
+        txtNome.style.color = "green"
+    }
+}
+
+function validaEmail(){
+    let txtEmail = document.querySelector('#txtEmail')
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.')){
+      txtEmail.innerHTML = "Email INVÁLIDO"
+      txtEmail.style.color = "red"
+    }else{
+        txtEmail.innerHTML = "Email VÁLIDO" 
+        txtEmail.style.color = "green"
+    }
+}
+
+function validaAssunto(){
+    let txtAssuntos = document.querySelector('#txtAssunto')
+    if(txtAssunto.value.lenght >=100){
+      txtAssunto.innerHTML = "Texto muito extenso, digite no máximo 100 caracteres"
+      txtAssunto.style.color = "red"
+      txtAssunto.style.display = 'block'
+    }else{
+        txtAssunto.style.display = 'none'
+    }
+}
